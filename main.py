d5af89e2ac460329bde291655c3ffa459455aed6
hussainsabir52 <hussucool0@gmail.com>
2021-12-22 11:48:01 +0500

    completion-engine

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import os
 from flask import Flask, request
 import openai
 import flask
+import random
+
+
 app = Flask(__name__)
 
 
@@ -13,23 +16,25 @@ def home():
 def completionEngine():
     #openai body parameters
     openai.api_key=request.headers.get('api_key');
+    # openai.organization=request.headers.get('organization')
     openai_engine=request.form['engine']
     openai_prompt=request.form['prompt']
     openai_temperature=request.form['temperature']
     openai_maxtokens=request.form['max_tokens']
     openai_stop=request.form['stop']
     
-    #completion endpoint
+    ## Completion Request
     response = openai.Completion.create(
     engine=openai_engine,
     prompt=openai_prompt,
     temperature=float(openai_temperature),
     max_tokens=int(openai_maxtokens),
-    stop=openai_stop
+    stop=openai_stop,
+    user=str(random.getrandbits(32))
     )
-    
     openai_content_to_verify = response['choices'][0]['text']
     
+    ## Toxicity Check
     filter_response = openai.Completion.create(
       engine="content-filter-alpha",
       prompt = "<|endoftext|>"+openai_content_to_verify+"\n--\nLabel:",
@@ -37,6 +42,8 @@ def completionEngine():
       max_tokens=1,
       logprobs=10
     )
+    
+    ## Toxicity Result Label
     filter_output_label=filter_response['choices'][0]['text']
     return flask.jsonify({"response": response, "toxicity":filter_output_label})
 
