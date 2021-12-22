67c595409870ee92fb0cdd3858172a57764ce4a5
hussainsabir52 <hussucool0@gmail.com>
2021-12-22 12:29:35 +0500

    completionModel

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def home():
     return "Hello Darkness My Old Friend!"
 
 @app.route("/completion-engine", methods=['POST'])
-def completionEngine():
+async def completionEngine():
     #openai body parameters
     openai.api_key=request.headers.get('api_key');
     # openai.organization=request.headers.get('organization')
@@ -24,8 +24,43 @@ def completionEngine():
     openai_stop=request.form['stop']
     
     ## Completion Request
-    response = openai.Completion.create(
+    response = await openai.Completion.create(
     engine=openai_engine,
+    prompt=openai_prompt,
+    temperature=float(openai_temperature),
+    max_tokens=int(openai_maxtokens),
+    stop=openai_stop,
+    user=str(random.getrandbits(32))
+    )
+    openai_content_to_verify = response['choices'][0]['text']
+    
+    ## Toxicity Check
+    filter_response = await openai.Completion.create(
+      engine="content-filter-alpha",
+      prompt = "<|endoftext|>"+openai_content_to_verify+"\n--\nLabel:",
+      temperature=0,
+      max_tokens=1,
+      logprobs=10
+    )
+    
+    ## Toxicity Result Label
+    filter_output_label=filter_response['choices'][0]['text']
+    return flask.jsonify({"response": response, "toxicity":filter_output_label})
+
+@app.route("/completion-model", methods=['POST'])
+def completionModel():
+    ## Openai body parameters
+    openai.api_key=request.headers.get('api_key');
+    openai.organization=request.headers.get('organization')
+    openai_model=request.form['model']
+    openai_prompt=request.form['prompt']
+    openai_temperature=request.form['temperature']
+    openai_maxtokens=request.form['max_tokens']
+    openai_stop=request.form['stop']
+    
+    ## Completion Request
+    response = openai.Completion.create(
+    model=openai_model,
     prompt=openai_prompt,
     temperature=float(openai_temperature),
     max_tokens=int(openai_maxtokens),
@@ -46,10 +81,6 @@ def completionEngine():
     ## Toxicity Result Label
     filter_output_label=filter_response['choices'][0]['text']
     return flask.jsonify({"response": response, "toxicity":filter_output_label})
-
-@app.route("/completion-model", methods=['POST'])
-def completionModel():
-    return flask.jsonify({"success":True})
 
 
 if __name__ == "__main__":
